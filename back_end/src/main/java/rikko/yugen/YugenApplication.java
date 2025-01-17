6b5ac28399f949a3de43930e51f57e9eb25dfc80
Rikko <bulletbill51@yahoo.com>
2025-01-17 00:50:30 -0800

    Spring boot backend created

---
+++ back_end/src/main/java/rikko/yugen/YugenApplication.java
@@ -0,0 +1,13 @@
+package rikko.yugen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugenApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/rikko/yugen/YugenApplicationTests.java
@@ -0,0 +1,13 @@
+package rikko.yugen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YugenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
