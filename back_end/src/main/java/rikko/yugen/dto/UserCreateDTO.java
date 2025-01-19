8689aade30d5c5f4cd08f8fd4acef8721dfb732d
Rikko <bulletbill51@yahoo.com>
2025-01-19 13:24:05 -0800

    Created validation checkers in usercreateDTO

--- back_end/src/main/java/rikko/yugen/dto/UserCreateDTO.java
+++ back_end/src/main/java/rikko/yugen/dto/UserCreateDTO.java
@@ -1,14 +1,27 @@
 package rikko.yugen.dto;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class UserCreateDTO {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
     private String image;
     
     //Getters and Setters
+    
     public String getUsername() {
         return username;
     }
