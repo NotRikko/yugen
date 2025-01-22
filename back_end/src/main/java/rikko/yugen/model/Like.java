d7c4eb3dd9dd4740a4f816ef39f495b2163330b9
Rikko <bulletbill51@yahoo.com>
2025-01-22 12:58:27 -0800

    Finished Like model

--- back_end/src/main/java/rikko/yugen/model/Like.java
+++ back_end/src/main/java/rikko/yugen/model/Like.java
@@ -21,5 +21,37 @@ public class Like {
 
     private LocalDateTime createdAt = LocalDateTime.now();
 
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
     
 }
