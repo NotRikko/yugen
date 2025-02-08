9fda458c47ee4af40dfb6932c99933b946d370dc
Rikko <bulletbill51@yahoo.com>
2025-02-08 13:22:31 -0800

    Signup form layout changed

--- front_end/src/components/SignupForm.tsx
+++ front_end/src/components/SignupForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -15,20 +16,43 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Checkbox } from "@/components/ui/checkbox"
+
  
 const formSchema = z.object({
   username: z.string().min(2).max(50),
+  email: z.string().email().min(5),
   password: z.string().min(8).max(50),
-  
+  confirm: z.string(),
+  isArtist: z.boolean(),
+  artistName: z.string().min(2).max(50).optional(),
 })
+.refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+})
+.refine((data) => {
+  if (data.isArtist && !data.artistName) return false;
+  return true;
+}, {
+  message: "Artist name is required",
+  path: ["artistName"],
+});
+
 
 export default function SignupForm() {
     const form = useForm<z.infer<typeof formSchema>>({
       resolver: zodResolver(formSchema),
       defaultValues: {
         username: "",
+        email: "",
+        password: "",
+        confirm: "",
+        isArtist: false,
+        artistName: "",
       },
     })
+    const [isArtist, setIsArtist] = useState(false);
    
     function onSubmit(values: z.infer<typeof formSchema>) {
       console.log(values)
@@ -36,7 +60,7 @@ export default function SignupForm() {
 
     return (
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-1">
             <FormField
               control={form.control}
               name="username"
@@ -44,10 +68,10 @@ export default function SignupForm() {
                 <FormItem>
                   <FormLabel>Username</FormLabel>
                   <FormControl>
-                    <Input placeholder="shadcn" {...field} />
+                    <Input placeholder="Example" {...field} />
                   </FormControl>
                   <FormDescription>
-                    This is your public display name.
+                    This is your username.
                   </FormDescription>
                   <FormMessage />
                 </FormItem>
@@ -55,12 +79,28 @@ export default function SignupForm() {
             />
             <FormField
               control={form.control}
-              name="username"
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="example@gmail.com" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Please input your email.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input placeholder="shadcn" {...field} />
+                    <Input type="password" placeholder="" {...field} />
                   </FormControl>
                   <FormDescription>
                     This is your password.
@@ -69,6 +109,47 @@ export default function SignupForm() {
                 </FormItem>
               )}
             />
+            <FormField
+              control={form.control}
+              name="confirm"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type ="password" placeholder="" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Please confirm password.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {isArtist ? 
+               <FormField
+               control={form.control}
+               name="artistName"
+               render={({ field }) => (
+                 <FormItem>
+                   <FormLabel>Artist Name</FormLabel>
+                   <FormControl>
+                     <Input placeholder="Artist" {...field} />
+                   </FormControl>
+                   <FormDescription>
+                      Please type your artist name.
+                   </FormDescription>
+                   <FormMessage />
+                 </FormItem>
+               )}
+             />
+            :
+            null}
+            <FormItem className="space-y-2 flex flex-row items-center space-x-2 gap-2">
+                <FormControl>
+                    <Checkbox checked={isArtist} onCheckedChange={(checked) => setIsArtist(checked === true)}  />
+                </FormControl>
+                <FormLabel className="!m-0">Signup as Artist?</FormLabel>
+            </FormItem>
             <Button type="submit">Submit</Button>
           </form>
         </Form>
--- front_end/src/pages/SignupPage.tsx
+++ front_end/src/pages/SignupPage.tsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar"
 import SignupForm from "../components/SignupForm"
 
 
@@ -8,9 +7,18 @@ function SignupPage():JSX.Element {
    
 
     return (
-        <div className="bg-slate-100 h-screen flex flex-col items-center">
-            <Navbar/>
-            <SignupForm />
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+            <div className="flex items-center  justify-center w-1/2 h-full m-20 border rounded-md">
+                <div className="p-8 w-1/2">
+                    <h1>Yugen</h1>
+                    <h2>Sign Up</h2>
+                    <SignupForm />
+                </div>
+                <img 
+                    src="https://64.media.tumblr.com/a30822675f02374d7ac7846e8172bc4d/d4ee3c438ce28514-0a/s540x810/cd48daea1fa30e2d18f6af2f62b721af76bdef42.png"
+                    className="h-full w-1/2"
+                />
+            </div>
         </div>
     )
 }
