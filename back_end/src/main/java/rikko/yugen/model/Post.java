a6256814d1cac3b6e4272f1d5644ac1fdaa72772
Rikko <bulletbill51@yahoo.com>
2025-02-13 17:40:06 -0800

    Refactored Post and User models

--- back_end/src/main/java/rikko/yugen/model/Post.java
+++ back_end/src/main/java/rikko/yugen/model/Post.java
@@ -15,6 +15,8 @@ public class Post {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Like> likes = new HashSet<>();
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
     
     @Column(nullable = false)
     private String content;
--- back_end/src/main/java/rikko/yugen/model/User.java
+++ back_end/src/main/java/rikko/yugen/model/User.java
@@ -35,6 +35,9 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Like> likes = new HashSet<>();
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
+
     // Spring Security Methods
 
     @Override
