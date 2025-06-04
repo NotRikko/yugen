9d024d0c2df8be7507d93e18fba49b54467cc4b9
Rikko <bulletbill51@yahoo.com>
2025-06-03 17:58:43 -0700

    FollowService created

--- back_end/src/main/java/rikko/yugen/repository/FollowRepository.java
+++ back_end/src/main/java/rikko/yugen/repository/FollowRepository.java
@@ -2,6 +2,7 @@ package rikko.yugen.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import rikko.yugen.model.Follow;
+import rikko.yugen.model.FollowId;
 import rikko.yugen.model.User;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     List<Follow> findByFollower(User follower);
     List<Follow> findByFollowee(User followee);
     boolean existsByFollowerAndFollowee(User follower, User followee);
+    boolean existsById(FollowId followId);
+    void deleteById(FollowId followId);
 }
