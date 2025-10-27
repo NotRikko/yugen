12f371f792c297f630da000991af37305b71e31c
Rikko <bulletbill51@yahoo.com>
2025-10-27 16:33:54 -0700

    Refactored back to use local storagea ccess token

--- front_end/src/shared/services/tokenService.ts
+++ front_end/src/shared/services/tokenService.ts
@@ -1,5 +1,5 @@
 export const tokenService = {
-    get: () => localStorage.getItem("accessToken"),
-    set: (token: string) => localStorage.setItem("accessToken", token),
-    remove: () => localStorage.removeItem("accessToken"),
-  };
+  get: () => localStorage.getItem("accessToken"),
+  set: (token: string) => localStorage.setItem("accessToken", token),
+  remove: () => localStorage.removeItem("accessToken"),
+};
