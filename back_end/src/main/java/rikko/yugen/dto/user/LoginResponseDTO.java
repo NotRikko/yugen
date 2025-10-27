5e0e6bf3a13c2c8179bdd3208a6389a85d7aac04
Rikko <bulletbill51@yahoo.com>
2025-10-26 22:09:34 -0700

    Refactored to use access and refresh tokens.

--- back_end/src/main/java/rikko/yugen/dto/user/LoginResponseDTO.java
+++ back_end/src/main/java/rikko/yugen/dto/user/LoginResponseDTO.java
@@ -1,22 +1,5 @@
 package rikko.yugen.dto.user;
 
-import rikko.yugen.model.LoginResponse;
-
-public class LoginResponseDTO {
-    private final String token;
-    private final Long expiresIn;
-
-    public LoginResponseDTO(LoginResponse loginResponse) {
-        this.token = loginResponse.getToken();
-        this.expiresIn = loginResponse.getExpiresIn();
-    }
-
-    // Getters
-    public String getToken() {
-        return token;
-    }
-
-    public Long getExpiresIn() {
-        return expiresIn;
-    }
+public record LoginResponseDTO(String accessToken, String refreshToken, Long accessTokenExpiresIn,
+                               Long refreshTokenExpiresIn) {
 }
