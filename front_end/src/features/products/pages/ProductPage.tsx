1569cbeeede15301b6260956911e803dd998abd3
Rikko <bulletbill51@yahoo.com>
2025-10-28 17:37:23 -0700

    Changed layout.

--- front_end/src/features/products/pages/ProductPage.tsx
+++ front_end/src/features/products/pages/ProductPage.tsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import { useProductHook } from "../hooks/useProduct";
 import ProductCreate from "../components/ProductCreate";
 import ProductGrid from "../components/ProductGrid";
+import { Button
 
+ } from "@mui/material";
 const ProductPage: React.FC = () => {
-  const { products, loading, error, createProductOptimistic, deleteProductOptimistic  } = useProductHook();
+  const { products, loading, error, createProductOptimistic, deleteProductOptimistic } = useProductHook();
+  const [showCreate, setShowCreate] = useState(false);
 
   if (loading) return <p>Loading products...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
+
   return (
-    <div className="py-12 px-24 grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <div className="lg:col-span-1">
-        <ProductCreate  onCreate={createProductOptimistic}/>
+    <div className="py-12 px-8 lg:px-24 flex flex-col gap-8">
+      <div className="flex flex-col items-center justify-between">
+        <h1 className="text-2xl font-bold">Products</h1>
+        <Button onClick={() => setShowCreate((prev) => !prev)}>
+          {showCreate ? "Cancel" : "Create Product"}
+        </Button>
       </div>
 
-      <div className="lg:col-span-2 flex flex-col gap-6">
-        <h1 className="text-2xl font-bold mb-4">Products</h1>
+      {showCreate && (
+        <div className="p-6 border rounded-2xl shadow-sm bg-white">
+          <ProductCreate onCreate={createProductOptimistic} />
+        </div>
+      )}
+
+      <div className="flex-1">
         <ProductGrid
           products={products}
           handleDelete={deleteProductOptimistic}
