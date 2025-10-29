cb6606a40fbf150cce6043ea91fe8ca6ec2a6a23
Rikko <bulletbill51@yahoo.com>
2025-10-29 14:07:49 -0700

    Simplified to use Jpa methods.

--- back_end/src/main/java/rikko/yugen/repository/LikeRepository.java
+++ back_end/src/main/java/rikko/yugen/repository/LikeRepository.java
@@ -12,11 +12,12 @@ import rikko.yugen.model.Like;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Set<Like> findByContentIdAndContentType(Long contentId, String contentType);
-    
+
     Optional<Like> findByUserIdAndContentIdAndContentType(Long userId, Long contentId, String contentType);
-    
+
     int countByContentIdAndContentType(Long contentId, String contentType);
 
-    @Query("SELECT l FROM Like l WHERE l.contentType = 'POST' AND l.contentId IN :postIds")
-    List<Like> findLikesForPosts(@Param("postIds") List<Long> postIds);
+    List<Like> findByContentTypeAndContentIdIn(String contentType, List<Long> postIds);
+
+    List<Like> findByContentTypeAndContentId(String contentType, Long postId);
 }
