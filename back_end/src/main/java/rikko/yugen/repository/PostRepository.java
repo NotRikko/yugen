18c2f0afe708f18274b4764a46f986913c972466
Rikko <bulletbill51@yahoo.com>
2025-10-28 17:31:39 -0700

    Removed unneeded queries.

--- back_end/src/main/java/rikko/yugen/repository/PostRepository.java
+++ back_end/src/main/java/rikko/yugen/repository/PostRepository.java
@@ -7,14 +7,10 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.domain.Pageable;
 
 import rikko.yugen.model.Post;
-import rikko.yugen.model.Product;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @EntityGraph(attributePaths = {"images", "artist", "product"})
-    List<Post> findAll();
     List<Post> findByArtist_Id(Long artistId);
     List<Post> findByArtist_ArtistName(String artistName);
     List<Post> findByArtist_IdIn(List<Long> artistIds, Pageable pageable);
-    List<Post> findByProduct(Product product);
-    List<Post> findByProductId(Long productId);
 }
