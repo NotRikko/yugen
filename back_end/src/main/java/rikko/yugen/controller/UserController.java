f3a0b71fc68f2e18b14bae450af856e404ce126c
Rikko <bulletbill51@yahoo.com>
2026-02-19 17:41:38 -0800

    Removed unused import.

--- back_end/src/main/java/rikko/yugen/controller/UserController.java
+++ back_end/src/main/java/rikko/yugen/controller/UserController.java
@@ -2,7 +2,6 @@ package rikko.yugen.controller;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
