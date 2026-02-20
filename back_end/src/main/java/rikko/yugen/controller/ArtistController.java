ba02989902544c2053a465bba675a1a023d69388
Rikko <bulletbill51@yahoo.com>
2026-02-19 17:01:23 -0800

    Refactored ArtistService and ArtistController

--- back_end/src/main/java/rikko/yugen/controller/ArtistController.java
+++ back_end/src/main/java/rikko/yugen/controller/ArtistController.java
@@ -3,6 +3,10 @@ package rikko.yugen.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +18,6 @@ import rikko.yugen.dto.artist.ArtistDTO;
 import rikko.yugen.dto.follow.FollowWithUserDTO;
 import rikko.yugen.dto.product.ProductDTO;
 import rikko.yugen.dto.post.PostDTO;
-import rikko.yugen.model.Artist;
 import rikko.yugen.service.ArtistService;
 import rikko.yugen.service.ProductService;
 import rikko.yugen.service.PostService;
@@ -31,31 +34,43 @@ public class ArtistController {
     private final PostService postService;
     private final FollowService followService;
 
-    @GetMapping("/")
-    public ResponseEntity<List<ArtistDTO>> getAllArtists() {
-        List<ArtistDTO> artists = artistService.getAllArtists()
-                .stream()
-                .map(ArtistDTO::new)
-                .collect(Collectors.toList());
+    // Artists
+
+    @GetMapping
+    public ResponseEntity<Page<ArtistDTO>> getAllArtists(
+            @PageableDefault(size = 10) Pageable pageable
+    ) {
+        Page<ArtistDTO> artists = artistService.getAllArtists(pageable);
         return ResponseEntity.ok(artists);
     }
 
-    @GetMapping("/artist")
-    public ResponseEntity<Artist> getArtist(@RequestParam String artistName) {
-        Artist artist = artistService.getArtistByName(artistName);
+    @GetMapping("/{artistId}")
+    public ResponseEntity<ArtistDTO> getArtistById(@PathVariable Long artistId) {
+        ArtistDTO artist = artistService.getArtistById(artistId);
         return ResponseEntity.ok(artist);
     }
 
-    @GetMapping("/name/{artistName}")
-    public ResponseEntity<ArtistDTO> getArtistByArtistName(@PathVariable String artistName) {
-        ArtistDTO artist = artistService.getArtistByArtistName(artistName);
+    @GetMapping("/by-name/{artistName}")
+    public ResponseEntity<ArtistDTO> getArtistByName(@PathVariable String artistName) {
+        ArtistDTO artist = artistService.getArtistByName(artistName);
         return ResponseEntity.ok(artist);
     }
+
+    @PostMapping
+    public ResponseEntity<ArtistDTO> createArtist(@Valid @RequestBody ArtistCreateDTO dto) {
+        ArtistDTO createdArtist = artistService.createArtist(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdArtist);
+    }
+
+    // Posts
 
     @GetMapping("/{artistId}/posts")
-    public List<PostDTO> getPostsByArtistId(@PathVariable Long artistId) {
-        return postService.getPostsByArtistId(artistId);
+    public ResponseEntity<List<PostDTO>> getPostsByArtistId(@PathVariable Long artistId) {
+        List<PostDTO> posts = postService.getPostsByArtistId(artistId);
+        return ResponseEntity.ok(posts);
     }
+
+    // Products
 
     @GetMapping("/{artistId}/products")
     public ResponseEntity<List<ProductDTO>> getProductsByArtistId(@PathVariable Long artistId) {
@@ -72,40 +87,28 @@ public class ArtistController {
         return ResponseEntity.ok(products);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<ArtistDTO> createArtist(@RequestBody ArtistCreateDTO artistCreateDTO) {
-        Artist createdArtist = artistService.createArtist(artistCreateDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ArtistDTO(createdArtist));
-    }
+    // Followers
 
     @PostMapping("/{artistId}/followers")
-    public ResponseEntity<FollowWithUserDTO> followArtist(
-            @PathVariable Long artistId
-    ) {
+    public ResponseEntity<FollowWithUserDTO> followArtist(@PathVariable Long artistId) {
         FollowWithUserDTO follow = followService.followArtist(artistId);
         return ResponseEntity.status(HttpStatus.CREATED).body(follow);
     }
 
     @DeleteMapping("/{artistId}/followers")
-    public ResponseEntity<Void> unfollowArtist(
-            @PathVariable Long artistId
-    ) {
+    public ResponseEntity<Void> unfollowArtist(@PathVariable Long artistId) {
         followService.unfollowArtist(artistId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{artistId}/followers")
-    public ResponseEntity<List<FollowWithUserDTO>> getFollowers(
-            @PathVariable Long artistId
-    ) {
+    public ResponseEntity<List<FollowWithUserDTO>> getFollowers(@PathVariable Long artistId) {
         List<FollowWithUserDTO> followers = followService.getFollowersForArtist(artistId);
         return ResponseEntity.ok(followers);
     }
 
-    @GetMapping("/{artistId}/check")
-    public ResponseEntity<Boolean> isFollowing(
-            @PathVariable Long artistId
-    ) {
+    @GetMapping("/{artistId}/followers/me")
+    public ResponseEntity<Boolean> isFollowing(@PathVariable Long artistId) {
         boolean following = followService.isFollowing(artistId);
         return ResponseEntity.ok(following);
     }
--- back_end/src/main/java/rikko/yugen/service/ArtistService.java
+++ back_end/src/main/java/rikko/yugen/service/ArtistService.java
@@ -4,9 +4,15 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
+import rikko.yugen.dto.user.UserDTO;
+import rikko.yugen.exception.ResourceAlreadyExistsException;
+import rikko.yugen.exception.ResourceNotFoundException;
 import rikko.yugen.repository.ArtistRepository;
 import rikko.yugen.repository.UserRepository;
 import rikko.yugen.dto.artist.ArtistCreateDTO;
@@ -17,47 +23,70 @@ import rikko.yugen.model.User;
 @Service
 @RequiredArgsConstructor
 public class ArtistService {
-    
+
     private final ArtistRepository artistRepository;
     private final UserRepository userRepository;
 
-    public Artist getArtistById(Long id) {
-        return artistRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Artist not found with id: " + id));
+    // Mapping
+    private ArtistDTO artistToArtistDTO(Artist artist) {
+        return new ArtistDTO(
+                artist.getId(),
+                artist.getArtistName(),
+                artist.getProfilePictureUrl(),
+                artist.getBannerPictureUrl(),
+                artist.getUser() != null ? new UserDTO(artist.getUser()) : null
+        );
     }
 
-    public Artist getArtistByName(String artistName) {
-        return artistRepository.findByArtistName(artistName)
-            .orElseThrow(() -> new RuntimeException("Artist not found with name: " + artistName));
+    // Read Operations
+
+    public ArtistDTO getArtistById(Long id) {
+        Artist artist = artistRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Artist", "id", id));
+
+        return artistToArtistDTO(artist);
     }
 
-    public ArtistDTO getArtistByArtistName(String artistName) {
+    public ArtistDTO getArtistByName(String artistName) {
         Artist artist = artistRepository.findByArtistName(artistName)
-                .orElseThrow(() -> new RuntimeException("Artist not found with name: " + artistName));
-        return new ArtistDTO(artist);
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Artist", "artistName", artistName));
+
+        return artistToArtistDTO(artist);
     }
 
-    public List<Artist> getAllArtists() {
-        return artistRepository.findAll();
+    public Page<ArtistDTO> getAllArtists(Pageable pageable) {
+        return artistRepository.findAll(pageable)
+                .map(this::artistToArtistDTO);
     }
+
+    // Create
 
     @Transactional
-    public Artist createArtist(ArtistCreateDTO artistCreateDTO) {
-        artistRepository.findByArtistName(artistCreateDTO.getArtistName())
-                .ifPresent(existingArtist -> {
-                    throw new RuntimeException("Artist with name '" + existingArtist.getArtistName() + "' already exists.");
-                });
+    public ArtistDTO createArtist(ArtistCreateDTO dto) {
 
-        User user = userRepository.findById(artistCreateDTO.getUserId())
-                                .orElseThrow(() -> new RuntimeException("User not found"));
+        String normalizedName = dto.getArtistName().trim();
+
+        if (artistRepository.existsByArtistName(normalizedName)) {
+            throw new ResourceAlreadyExistsException("Artist", "artistName", normalizedName);
+        }
+
+        User user = userRepository.findById(dto.getUserId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User", "id", dto.getUserId()));
 
         Artist artist = new Artist();
-        artist.setArtistName(artistCreateDTO.getArtistName());
-        artist.setProfilePictureUrl(artistCreateDTO.getProfilePictureUrl());
-        artist.setBannerPictureUrl(artistCreateDTO.getBannerPictureUrl());
-        artist.setUser(user); 
+        artist.setArtistName(normalizedName);
+        artist.setProfilePictureUrl(dto.getProfilePictureUrl());
+        artist.setBannerPictureUrl(dto.getBannerPictureUrl());
+        artist.setUser(user);
 
-        return artistRepository.save(artist);
-
+        try {
+            Artist savedArtist = artistRepository.save(artist);
+            return artistToArtistDTO(savedArtist);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResourceAlreadyExistsException("Artist", "artistName", normalizedName);
+        }
     }
 }
