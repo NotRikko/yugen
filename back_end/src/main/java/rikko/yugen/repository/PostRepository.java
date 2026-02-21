6f07d24d64a7a31279d0faf6254b86116b0ef620
Rikko <bulletbill51@yahoo.com>
2026-02-21 13:58:29 -0800

    Changed type from List to Page.

--- back_end/src/main/java/rikko/yugen/repository/PostRepository.java
+++ back_end/src/main/java/rikko/yugen/repository/PostRepository.java
@@ -2,6 +2,7 @@ package rikko.yugen.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.domain.Pageable;
@@ -12,5 +13,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @EntityGraph(attributePaths = {"images", "artist", "product"})
     List<Post> findByArtist_Id(Long artistId);
     List<Post> findByArtist_ArtistName(String artistName);
-    List<Post> findByArtist_IdIn(List<Long> artistIds, Pageable pageable);
+    Page<Post> findByArtist_IdIn(List<Long> artistIds, Pageable pageable);
 }
