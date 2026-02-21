e40f62bb460b04a623488f7ce20798d6f80a4e2f
Rikko <bulletbill51@yahoo.com>
2026-02-21 12:35:08 -0800

    Removed unused import.

--- back_end/src/main/java/rikko/yugen/service/UserService.java
+++ back_end/src/main/java/rikko/yugen/service/UserService.java
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import rikko.yugen.dto.user.UserDTO;
 import rikko.yugen.exception.*;
