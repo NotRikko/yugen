a5e5b0a8a4f0bbb8e2d22b723bcb85401331567b
Rikko <bulletbill51@yahoo.com>
2026-02-22 13:48:09 -0800

    Refactored to use pages rather than lists.

--- back_end/src/main/java/rikko/yugen/repository/ProductRepository.java
+++ back_end/src/main/java/rikko/yugen/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package rikko.yugen.repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import rikko.yugen.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
-    Product findByName(String name);
-    List<Product> findByArtist_ArtistName(String artistName);
-    List<Product> findByArtistId(Long artistId);
-    List<Product> findByCollections_Name(String collectionName);
+    Page<Product> findByArtist_ArtistName(String artistName, Pageable pageable);
+    Page<Product> findByArtistId(Long artistId, Pageable pageable);
+    Page<Product> findByCollections_Name(String collectionName, Pageable pageable);
+    Page<Product> findByName(String name, Pageable pageable);
 }
