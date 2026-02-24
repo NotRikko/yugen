eddd11ab0dcd250900697ad041f0a9afdc89b3bd
Rikko <bulletbill51@yahoo.com>
2026-02-24 14:10:35 -0800

    New pattern annotation regex to ensure password strength is met.

--- back_end/src/main/java/rikko/yugen/dto/user/UserCreateDTO.java
+++ back_end/src/main/java/rikko/yugen/dto/user/UserCreateDTO.java
@@ -1,6 +1,7 @@
 package rikko.yugen.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,6 +23,10 @@ public class UserCreateDTO {
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).+$",
+            message = "Password must contain at least 1 number and 1 special character"
+    )
     private String password;
 
     private Boolean isArtist;
