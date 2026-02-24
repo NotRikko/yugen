1c55cea1e3f7fda4586073ff9e175bd1bd332b52
Rikko <bulletbill51@yahoo.com>
2026-02-23 23:06:39 -0800

    Added entity graph annotation to prevent query explosions.

--- back_end/src/main/java/rikko/yugen/repository/UserRepository.java
+++ back_end/src/main/java/rikko/yugen/repository/UserRepository.java
@@ -1,15 +1,28 @@
 package rikko.yugen.repository;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import rikko.yugen.model.User;
 
+public interface UserRepository extends JpaRepository<User, Long> {
 
-public interface UserRepository extends JpaRepository<User, Long>{
+    @EntityGraph(attributePaths = {"artist", "profileImage"})
     Optional<User> findByDisplayName(String displayName);
+
+    @EntityGraph(attributePaths = {"artist", "profileImage"})
     Optional<User> findByUsername(String username);
+
+    @EntityGraph(attributePaths = {"artist", "profileImage"})
     Optional<User> findByEmail(String email);
+
+    @Override
+    @EntityGraph(attributePaths = {"artist", "profileImage"})
+    Page<User> findAll(Pageable pageable);
+
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
