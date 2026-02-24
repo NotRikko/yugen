670475883c8ae76fd90c2a6074d3f9784b732e58
Rikko <bulletbill51@yahoo.com>
2026-02-24 15:06:18 -0800

    Implemented not blank annotation validation checks.

--- back_end/src/main/java/rikko/yugen/dto/user/UserLoginDTO.java
+++ back_end/src/main/java/rikko/yugen/dto/user/UserLoginDTO.java
@@ -1,12 +1,17 @@
 package rikko.yugen.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class UserLoginDTO {
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 
 }
