7b2bd8ad9538fb93c3f36461ff13db709920d514
Rikko <bulletbill51@yahoo.com>
2026-02-23 23:06:26 -0800

    Test docker creation.

---
+++ back_end/src/test/java/rikko/yugen/integration/TestDocker.java
@@ -0,0 +1,9 @@
+package rikko.yugen.integration;
+
+import org.testcontainers.DockerClientFactory;
+
+public class TestDocker {
+    public static void main(String[] args) {
+        System.out.println(DockerClientFactory.instance().client());
+    }
+}
