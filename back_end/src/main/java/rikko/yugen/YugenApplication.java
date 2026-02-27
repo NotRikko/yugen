9dde7aeff569afb570f53a4c52f4d5e6eb2cd9b8
Rikko <bulletbill51@yahoo.com>
2026-02-26 16:10:38 -0800

    Enabled JpaAuditing to allow for base model class to share fields with other models.

--- back_end/src/main/java/rikko/yugen/YugenApplication.java
+++ back_end/src/main/java/rikko/yugen/YugenApplication.java
@@ -4,10 +4,12 @@ package rikko.yugen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @EnableJpaRepositories("rikko.yugen.repository")
+@EnableJpaAuditing
 @SpringBootApplication
 (exclude = {SecurityAutoConfiguration.class})
 public class YugenApplication {
