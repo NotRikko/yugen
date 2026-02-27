433180f12c5b869327210e83e49fdd1c24412194
Rikko <bulletbill51@yahoo.com>
2026-02-27 11:11:45 -0800

    Created repository method to query by usrId with eager fetching on cart items.

--- back_end/src/main/java/rikko/yugen/repository/CartRepository.java
+++ back_end/src/main/java/rikko/yugen/repository/CartRepository.java
@@ -3,8 +3,13 @@ package rikko.yugen.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rikko.yugen.model.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserId(Long userId);
+
+    @Query("SELECT c FROM Cart c LEFT JOIN FETCH c.items WHERE c.user.id = :userId")
+    Optional<Cart> findByUserIdWithItems(@Param("userId") Long userId);
 }
