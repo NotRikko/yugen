828fc33f073d014094be998ced31c62fba1ce948
Rikko <bulletbill51@yahoo.com>
2026-02-27 10:44:40 -0800

    Created DTOs for cart add and cart update item requests.

---
+++ back_end/src/main/java/rikko/yugen/dto/cart/CartUpdateItemDTO.java
@@ -0,0 +1,18 @@
+package rikko.yugen.dto.cart;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartUpdateItemDTO {
+    @NotNull
+    private Long cartItemId;
+
+    @Min(1)
+    private int quantity;
+}
