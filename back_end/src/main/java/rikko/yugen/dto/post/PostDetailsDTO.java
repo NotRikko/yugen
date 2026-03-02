3ec73e9aab2cdc1193453b2a486adf9e2353d215
Rikko <bulletbill51@yahoo.com>
2026-03-02 15:37:27 -0800

    Refactored type to page instead of list to match repository return type.

--- back_end/src/main/java/rikko/yugen/dto/post/PostDetailsDTO.java
+++ back_end/src/main/java/rikko/yugen/dto/post/PostDetailsDTO.java
@@ -1,7 +1,9 @@
 package rikko.yugen.dto.post;
 
+import org.springframework.data.domain.Page;
 import rikko.yugen.dto.artist.ArtistSummaryDTO;
 import rikko.yugen.dto.comment.CommentDTO;
+import rikko.yugen.model.Comment;
 import rikko.yugen.model.Image;
 import rikko.yugen.model.Post;
 
@@ -17,25 +19,21 @@ public record PostDetailsDTO(
         LocalDateTime createdAt,
         LocalDateTime updatedAt,
         ArtistSummaryDTO artist,
-        List<CommentDTO> comments,
-        Long productId
+        Long productId,
+        Page<CommentDTO> comments
 ) {
-    public PostDetailsDTO(Post post) {
+    public PostDetailsDTO(Post post, Page<Comment> commentsPage) {
         this(
                 post.getId(),
                 post.getContent(),
                 post.getLikes() != null ? post.getLikes().size() : 0,
                 post.getComments() != null ? post.getComments().size() : 0,
-                post.getImages() != null
-                        ? post.getImages().stream().map(Image::getUrl).toList()
-                        : List.of(),
+                post.getImages() != null ? post.getImages().stream().map(Image::getUrl).toList() : List.of(),
                 post.getCreatedAt(),
                 post.getUpdatedAt(),
                 post.getArtist() != null ? new ArtistSummaryDTO(post.getArtist()) : null,
-                post.getComments() != null
-                        ? post.getComments().stream().map(CommentDTO::new).toList()
-                        : List.of(),
-                post.getProduct() != null ? post.getProduct().getId() : null
+                post.getProduct() != null ? post.getProduct().getId() : null,
+                commentsPage.map(CommentDTO::new)
         );
     }
 }
