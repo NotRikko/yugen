81f1481e8bef7ca419742030957a8193b00accf2
Rikko <bulletbill51@yahoo.com>
2026-03-02 15:18:15 -0800

    Removed old types and used scehma types from back end from openAPI generation.

--- front_end/src/features/comments/types/commentTypes.ts
+++
@@ -1,8 +0,0 @@
-import type { PartialUser } from "@/features/user/types/userTypes";
-
-export interface PartialComment {
-    id: number;
-    user: PartialUser;
-    content: string;
-    postedAt: string;
-}
