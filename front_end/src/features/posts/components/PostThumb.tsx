0e7c10b6525cb082137b07aabbcc1a8843a2657e
Rikko <bulletbill51@yahoo.com>
2026-03-03 14:14:22 -0800

    Refactored to have a fallback image.

--- front_end/src/features/posts/components/PostThumb.tsx
+++ front_end/src/features/posts/components/PostThumb.tsx
@@ -1,15 +1,25 @@
-import type { PostDTO } from "@/features/posts/types/postTypes";
+import type { PostDTO } from "../types";
 
 interface PostThumbProps {
-    post: PostDTO;
+  post: PostDTO;
 }
-
 
 function PostThumb({ post }: PostThumbProps) {
-    return (
-        <div className="p-4 border">
-            <img src={post.images[0].url} alt={post.content} />
-        </div>
-    );
+  const firstImage = post.imageUrls?.[0] ?? "https://i.pinimg.com/736x/18/c2/f7/18c2f7a303ad5b05d8a41c6b7e4c062b.jpg"; 
+
+  return (
+    <div className="p-4 border">
+      <img
+        src={firstImage}
+        alt={post.content || "Post image"}
+        className="w-full h-32 object-cover rounded-md"
+        onError={(e) => {
+          e.currentTarget.src =
+            "https://i.pinimg.com/736x/18/c2/f7/18c2f7a303ad5b05d8a41c6b7e4c062b.jpg";
+        }}
+      />
+    </div>
+  );
 }
-export default PostThumb
+
+export default PostThumb;
