cec11f9b6e46e1991f0c40ccd38d40ba6f82ab2e
Rikko <bulletbill51@yahoo.com>
2026-03-03 12:33:47 -0800

    Refactored to return null if no current user/guest user.

--- back_end/src/main/java/rikko/yugen/helpers/CurrentUserHelper.java
+++ back_end/src/main/java/rikko/yugen/helpers/CurrentUserHelper.java
@@ -1,40 +1,36 @@
 package rikko.yugen.helpers;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import rikko.yugen.exception.ResourceNotFoundException;
 import rikko.yugen.model.User;
 import rikko.yugen.repository.UserRepository;
 
 @Component
-@RequiredArgsConstructor
 public class CurrentUserHelper {
 
     private final UserRepository userRepository;
 
+    public CurrentUserHelper(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || !authentication.isAuthenticated()) {
-            throw new AuthenticationCredentialsNotFoundException("User not authenticated");
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return null;
         }
 
         Object principal = authentication.getPrincipal();
-        String username;
-
         if (principal instanceof UserDetails userDetails) {
-            username = userDetails.getUsername();
-        } else {
-            throw new AuthenticationCredentialsNotFoundException("User not authenticated");
+            String username = userDetails.getUsername();
+            return userRepository.findByUsername(username).orElse(null);
         }
 
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new ResourceNotFoundException("User ", "username", username));
+        return null;
     }
 }
