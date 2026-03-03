71050f58d1d2029b913c9d502c33fb3e6044019d
Rikko <bulletbill51@yahoo.com>
2026-03-03 14:14:05 -0800

    Added new method to make api call to product by id. Refactored create product to handle new type.

--- front_end/src/features/products/api/productApi.ts
+++ front_end/src/features/products/api/productApi.ts
@@ -1,9 +1,8 @@
 import { fetchClient } from "@/shared/api/fetchClient";
-import type { Product } from "../types/productTypes";
+import type { ProductDTO } from "../types";
 
 export const productApi = {
     createProduct: async (data: {
-        artistId: number | null;
         name: string;
         quantityInStock: number;
         description: string;
@@ -11,40 +10,37 @@ export const productApi = {
         seriesIds: number[];
         collectionIds: number[];
         files?: File[];
-    }) : Promise<Product> => {
+      }): Promise<ProductDTO> => {
         const formData = new FormData();
-
+      
         const productData = {
-            name: data.name,
-            description: data.description,
-            price: data.price,
-            quantityInStock: data.quantityInStock,
-            seriesIds: data.seriesIds ?? [],
-            collectionIds: data.collectionIds ?? [],
+          name: data.name,
+          description: data.description,
+          price: data.price,
+          quantityInStock: data.quantityInStock,
+          seriesIds: data.seriesIds ?? [],
+          collectionIds: data.collectionIds ?? [],
         };
-
+      
         formData.append(
-            "product",
-            new Blob([JSON.stringify(productData)], { type: "application/json"})
+          "product",
+          new Blob([JSON.stringify(productData)], { type: "application/json" })
         );
-
-        if (data.files && data.files.length > 0) {
-            data.files.forEach((file) => formData.append("files", file));
-        }
-
-        const result = await fetchClient<Product>("/products/" , {
-            method: "POST",
-            body: formData,
-            auth: true,
+      
+        (data.files ?? []).forEach((file) => formData.append("files", file));
+      
+        const result = await fetchClient<ProductDTO>("/products/", {
+          method: "POST",
+          body: formData,
+          auth: true,
         });
-
+      
         if (!result) throw new Error("Failed to create product");
-
-        return result
+        return result;
     },
 
-    fetchCurrentArtistProducts: async () : Promise<Product[]> => {
-        const products = await fetchClient<Product[]>("/artists/me/products", {
+    fetchCurrentArtistProducts: async () : Promise<ProductDTO[]> => {
+        const products = await fetchClient<ProductDTO[]>("/artists/me/products", {
             method: "GET",
             auth: true
         });
@@ -53,8 +49,11 @@ export const productApi = {
         return products;
     },
 
+    getProductById: ( id: number) =>
+        fetchClient<ProductDTO>(`products/${id}`, { method: "GET" }),
+
     getProductsByArtistId: (artistId: number) =>
-        fetchClient<Product[]>(`/artists/${artistId}/products`, { method: "GET" }),
+        fetchClient<ProductDTO[]>(`/artists/${artistId}/products`, { method: "GET" }),
     
     deleteProduct: async (productId: number) : Promise<void> => {
         await fetchClient<void>(`/products/${productId}`, {
