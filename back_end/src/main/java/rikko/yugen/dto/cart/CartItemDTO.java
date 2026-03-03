a58ecccb9c87a0a56efee0ed5c0cd8b3cde7b670
Rikko <bulletbill51@yahoo.com>
2026-03-02 17:01:49 -0800

    Refactored to use ProductSummaryDTO

--- back_end/src/main/java/rikko/yugen/dto/cart/CartItemDTO.java
+++ back_end/src/main/java/rikko/yugen/dto/cart/CartItemDTO.java
@@ -1,21 +1,17 @@
 package rikko.yugen.dto.cart;
 
+import rikko.yugen.dto.product.ProductSummaryDTO;
 import rikko.yugen.model.CartItem;
-import rikko.yugen.dto.product.ProductDTO;
 
 public record CartItemDTO(
         Long id,
-        Long productId,
-        String productName,
-        Float productPrice,
+        ProductSummaryDTO product,
         int quantity
 ) {
     public CartItemDTO(CartItem cartItem) {
         this(
                 cartItem.getId(),
-                cartItem.getProduct() != null ? cartItem.getProduct().getId() : null,
-                cartItem.getProduct() != null ? cartItem.getProduct().getName() : null,
-                cartItem.getProduct() != null ? cartItem.getProduct().getPrice() : null,
+                cartItem.getProduct() != null ? new ProductSummaryDTO(cartItem.getProduct()) : null,
                 cartItem.getQuantity()
         );
     }
