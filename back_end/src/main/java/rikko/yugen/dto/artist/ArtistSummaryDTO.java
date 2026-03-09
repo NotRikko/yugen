4175a79e06aa48874ae3c37f4f055dddda36819b
Rikko <bulletbill51@yahoo.com>
2026-03-08 22:24:46 -0700

    Refactored artist summary dto to include user display name.

--- back_end/src/main/java/rikko/yugen/dto/artist/ArtistSummaryDTO.java
+++ back_end/src/main/java/rikko/yugen/dto/artist/ArtistSummaryDTO.java
@@ -5,12 +5,14 @@ import rikko.yugen.model.Artist;
 public record ArtistSummaryDTO(
         Long id,
         String username,
+        String displayName,
         String profilePictureUrl
 ) {
     public ArtistSummaryDTO(Artist artist) {
         this(
                 artist.getId(),
                 artist.getUser().getUsername(),
+                artist.getUser().getDisplayName(),
                 artist.getProfileImage() != null ? artist.getProfileImage().getUrl() : null
         );
     }
